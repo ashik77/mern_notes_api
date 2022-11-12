dcddb21efa3ccacf3a9cc45114934ecebc6763fa
MD ASHIQUR RAHMAN <ashikaman77@gmail.com>
2022-11-12 11:55:32 +0200

    root router and controller added

---
+++ controllers/root.controller.js
@@ -0,0 +1,25 @@
+import path from "path";
+const __dirname = path.resolve(); //otherwise ref error as using module type
+
+export const getRoot = async (req, res) => {
+  try {
+    await res.sendFile(path.join(__dirname, "views", "index.html"));
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+export const notFound = async (req, res) => {
+  try {
+    res.status(404);
+    if (req.accepts("html")) {
+      await res.sendFile(path.join(__dirname, "views", "notFound.html"));
+    } else if (req.accepts("json")) {
+      res.json({ message: "404 Not Found" });
+    } else {
+      res.type("txt").send("404 Not Found");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
--- routes/root.router.js
+++ routes/root.router.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getRoot } from "./controllers/root.controller.js";
+import { getRoot, notFound } from "../controllers/root.controller.js";
 
 const router = express.Router();
 
 router.get("^/$|/index(.html)?", getRoot);
 
+router.all("*", notFound);
 export default router;

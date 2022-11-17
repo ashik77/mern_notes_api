d7a0dcdcf2874d80e48e35a63dc3cbd540ea2d58
MD ASHIQUR RAHMAN <ashikaman77@gmail.com>
2022-11-17 10:48:35 +0200

    refactoring

--- controllers/note.controller.js
+++ controllers/note.controller.js
@@ -39,7 +39,10 @@ export const createNewNote = asyncHandler(async (req, res) => {
   }
 
   // Check for duplicate title
-  const duplicate = await Note.findOne({ title }).lean().exec();
+  const duplicate = await Note.findOne({ title })
+    .collation({ locale: "en", strength: 2 })
+    .lean()
+    .exec();
 
   if (duplicate) {
     return res.status(409).json({ message: "Duplicate note title" });
@@ -75,7 +78,10 @@ export const updateNote = asyncHandler(async (req, res) => {
   }
 
   // Check for duplicate title
-  const duplicate = await Note.findOne({ title }).lean().exec();
+  const duplicate = await Note.findOne({ title })
+    .collation({ locale: "en", strength: 2 })
+    .lean()
+    .exec();
 
   // Allow renaming of the original note
   if (duplicate && duplicate?._id.toString() !== id) {
